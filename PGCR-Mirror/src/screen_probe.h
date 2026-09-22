77199c2ca172298ef47ad3df650933943ea081a2
fuyunnat <132661849+fuyunnat@users.noreply.github.com>
2026-09-23 01:32:24 +0800

    feat(pgcr v0.4): add read-only Screen source probe header

---
+++ PGCR-Mirror/src/screen_probe.h
@@ -0,0 +1,8 @@
+#ifndef SCREEN_PROBE_H
+#define SCREEN_PROBE_H
+
+/* Read-only QNX Screen inventory. Does not create, move, resize, join,
+ * destroy, or otherwise mutate existing vehicle windows. */
+int pgcr_run_screen_probe();
+
+#endif
